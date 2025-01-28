7fec9f19befb42459d124044bbd605e667d4e221
Baldo <barrosoub@protonmail.com>
2025-01-28 11:57:23 -0600

    bug fixed

--- app/api/users/delete/route.ts
+++ app/api/users/delete/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   try {
     const users = await prisma.user.findMany();
     return NextResponse.json(users, { status: 200 });
