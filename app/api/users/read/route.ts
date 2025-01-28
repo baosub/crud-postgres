b8eb7ced0c25a7e9e7efd61be33c85de0ca9a201
Baldo <barrosoub@protonmail.com>
2025-01-28 10:00:52 -0600

    The route was corrected

--- app/api/users/read/route.ts
+++ app/api/users/read/route.ts
@@ -1,10 +1,9 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   const users = await prisma.user.findMany();
   return NextResponse.json(users, { status: 200 });
 }
-
